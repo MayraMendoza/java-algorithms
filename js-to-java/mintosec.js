379e1ad3d16e2deda44a2498337e1540c16b18f5
MayraMendoza <mayra601m@gmail.com>
2021-12-15 19:46:32 -0500

    completed js min to sec

---
+++ js-to-java/mintosec.js
@@ -0,0 +1,8 @@
+//Write a function that takes an integer minutes and converts it to seconds.
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+function convert(minutes) {
+	seconds = minutes * 60
+	return seconds;
+	
+}
