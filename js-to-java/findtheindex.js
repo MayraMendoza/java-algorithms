2422efb792bfc6ee285422c40aebfcfafa79f2d8
MayraMendoza <mayra601m@gmail.com>
2021-12-15 19:36:05 -0500

    completed js find the index

---
+++ js-to-java/findtheindex.js
@@ -0,0 +1,7 @@
+//https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+//Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+
+function search(arr, item) {
+    spot = arr.indexOf(item)
+        return spot;
+    }
