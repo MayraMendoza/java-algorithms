f7c7b70dab01aea5ea4c96ed36e3dceb4e81f52c
MayraMendoza <mayra601m@gmail.com>
2021-12-15 19:42:55 -0500

    completed js returnsomething

---
+++ js-to-java/returnsomthing.js
@@ -0,0 +1,8 @@
+//https://edabit.com/challenge/MvZK536X7fyrWH8Qc
+
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+function giveMeSomething(a) {
+	a = "something"+ " " + a;
+	return a;
+}
